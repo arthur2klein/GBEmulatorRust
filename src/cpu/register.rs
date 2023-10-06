b37f03d24b0a2c504f35525ee45919f99a030f2c
Pasteqk <toniovacossin98@gmail.com>
2023-10-06 15:11:18 +0200

    instructions

--- src/cpu/register.rs
+++ src/cpu/register.rs
@@ -96,3 +96,66 @@ impl std::convert::From<u8> for FlagsRegister {
 }
 /* ---------------------------------------------------------------------------*/
 
+//Instruction 
+
+enum Instruction {
+    ADD(ArithmeticTarget),
+  }
+  
+  enum ArithmeticTarget {
+    A, B, C, D, E, H, L,
+  }
+
+//On execute les instructions en implemetant CPU
+
+impl CPU {
+  fn execute(&mut self, instruction: Instruction) {
+    match instruction {
+      Instruction::ADD(target) => {
+        match target {
+          ArithmeticTarget::A => {
+            let value = self.registers.a;
+            let new_value = self.add(value);
+            self.registers.a = new_value;
+          }
+          ArithmeticTarget::B => {
+            let value = self.registers.b;
+            let new_value = self.add(value);
+            self.registers.a = new_value;
+          }
+          ArithmeticTarget::C => {
+            let value = self.registers.c;
+            let new_value = self.add(value);
+            self.registers.a = new_value;
+          }
+          ArithmeticTarget::D => {
+            let value = self.registers.d;
+            let new_value = self.add(value);
+            self.registers.a = new_value;
+          }
+          ArithmeticTarget::E => {
+            let value = self.registers.e;
+            let new_value = self.add(value);
+            self.registers.a = new_value;
+          }
+          ArithmeticTarget::H => {
+            let value = self.registers.h;
+            let new_value = self.add(value);
+            self.registers.a = new_value;
+          }
+          ArithmeticTarget::L => {
+            let value = self.registers.l;
+            let new_value = self.add(value);
+            self.registers.a = new_value;
+          }
+        }
+      }
+      _ => { /* TODO: support more instructions */ }
+    }
+  }
+  fn add(&mut self, value: u8) -> u8 {
+    let (new_value, did_overflow) = self.registers.a.overflowing_add(value);
+    // TODO: set flags
+    new_value
+  }
+}
